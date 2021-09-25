da5970bfa49ef6712e72ed2640cfcb8616ee811c
Cryolin <siyu@mail.ustc.edu.cn>
2021-09-25 21:52:20 +0800

    AudioTrack

---
+++ demo10/app/src/main/java/colin/com/audiotrackdemo/MainActivity.java
@@ -0,0 +1,15 @@
+package colin.com.audiotrackdemo;
+
+import android.media.AudioAttributes;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AudioAttributes attributes = new AudioAttributes();
+    }
+}
